a1eb961d3e8230d47a1bf1866e5427694b0867ee
zhangning37 <zhangning37@baidu.com>
2025-12-16 11:07:30 +0800

    remove dir

---
+++ arun/bin/install_grafana_mac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+curl -O https://dl.grafana.com/grafana-enterprise/release/12.3.0/grafana-enterprise_12.3.0_19497075765_darwin_arm64.tar.gz
--- arun/grafana/install_grafana_mac.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-curl -O https://dl.grafana.com/grafana/release/12.2.0/grafana_12.2.0_17949786146_darwin_amd64.tar.gz
